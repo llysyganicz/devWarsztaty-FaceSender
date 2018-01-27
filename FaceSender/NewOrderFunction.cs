e7804bf29814e65a2f48403e65f692bdeaa87b75
Łukasz Łysyganicz <llysyganicz@gmail.com>
2018-01-27 11:48:29 +0100

    Storage

--- FaceSender/NewOrderFunction.cs
+++ FaceSender/NewOrderFunction.cs
@@ -5,20 +5,26 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
+using Microsoft.WindowsAzure.Storage;
 
 namespace FaceSender
 {
     public static class NewOrderFunction
     {
         [FunctionName("NewOrderFunction")]
-        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]HttpRequest req, TraceWriter log)
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]HttpRequest req, 
+            [Table("Order", Connection = "StorageConnection")]ICollector<OrderDetails> orders,
+            TraceWriter log)
         {
             string requestBody = new StreamReader(req.Body).ReadToEnd();
             var orderDetails = JsonConvert.DeserializeObject<OrderDetails>(requestBody);
 
-            return orderDetails != null
-                ? (ActionResult)new OkObjectResult("Order saved.")
-                : new BadRequestObjectResult("Please pass a valid order in the request body");
+            if (orderDetails != null)
+            {
+                orders.Add(orderDetails);
+                return new OkObjectResult("Order saved.");
+            }
+            return new BadRequestObjectResult("Please pass a valid order in the request body");
         }
     }
 
