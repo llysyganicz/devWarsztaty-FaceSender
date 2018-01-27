b372c289af28feb2190e4d89b3b45fc0d45669a0
Łukasz Łysyganicz <llysyganicz@gmail.com>
2018-01-27 13:08:50 +0100

    Change retrieve function implementation.

--- FaceSender/RetrieveOrderFunction.cs
+++ FaceSender/RetrieveOrderFunction.cs
@@ -2,8 +2,10 @@ using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table;
@@ -13,27 +15,26 @@ namespace FaceSender
     public static class RetrieveOrderFunction
     {
         [FunctionName("RetrieveOrderFunction")]
-        public async static Task<IActionResult> Run([BlobTrigger("photos/{name}", Connection = "StorageConnection")]Stream myBlob, string name, TraceWriter log)
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req,
+            [Table("Orders", Connection = "StorageConnection")]CloudTable ordersTable, TraceWriter log)
         {
-            log.Info($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+            string fileName = req.Query["fileName"];
+            if (string.IsNullOrWhiteSpace(fileName))
+                return new BadRequestResult();
+            TableQuery<OrderDetails> query = new TableQuery<OrderDetails>().Where(TableQuery.GenerateFilterCondition("RowKey", QueryComparisons.Equal, fileName));
+            TableQuerySegment<OrderDetails> tableQueryResult = await ordersTable.ExecuteQuerySegmentedAsync(query, null);
+            var resultList = tableQueryResult.Results;
 
-            var storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnection"]);
-            var tableClient = storageAccount.CreateCloudTableClient();
-            var orderTable = tableClient.GetTableReference("Order");
-            var query = new TableQuery<OrderDetails>().Where(TableQuery.GenerateFilterCondition("RowKey", QueryComparisons.Equal, name));
-            var result = await orderTable.ExecuteQuerySegmentedAsync<OrderDetails>(query, null);
-            var tableResult = result.Results;
-
-            if(tableResult.Any())
+            if (resultList.Any())
             {
-                var first = tableResult.First();
+                var firstElement = resultList.First();
                 return new JsonResult(new
                 {
-                    first.CustomerEmail,
-                    first.CustomerName,
-                    first.PhotoHeight,
-                    first.PhotoWidth,
-                    first.PhotoName
+                    firstElement.CustomerName,
+                    firstElement.CustomerEmail,
+                    firstElement.PhotoName,
+                    firstElement.PhotoHeight,
+                    firstElement.PhotoWidth
                 });
             }
 
